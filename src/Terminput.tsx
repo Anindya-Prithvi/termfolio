087381b7093e69c7a67002a386cae6a090bed3b5
Andy Candy <anindya20024@iiitd.ac.in>
2022-07-15 13:48:08 +0530

    consume terminal input

--- src/Terminput.tsx
+++ src/Terminput.tsx
@@ -6,16 +6,22 @@ const Terminput = () => {
     const [width, setWidth] = useState(0);
     const changeHandler = evt => {
         if (evt.key === 'Backspace') { setWidth(evt.target.value.length - 1) }
+        else if (evt.key === 'Enter') {
+            console.log("not impl");
+            evt.preventDefault();
+            const inpelement = document.getElementById("terminput") as HTMLInputElement;
+            inpelement.value = "";
+            setWidth(0);
+        }
         else if (evt.target.value.length >= 30) {
             console.log("not impl");
             console.log("Terminal bell, input too long");
             evt.preventDefault();
         }
-        else if (evt.key === 'Enter') { console.log("not impl"); }
         else if (evt.nativeEvent.key.length === 1) { setWidth(evt.target.value.length + 1) };
     };
 
-    return (<div className={styles.terminalcontainer}>{getColored("visitor14234@online:$ ", "#f48020")}<input style={{ width: width + 'ch' }} className={styles.terminput} autoFocus onKeyDown={changeHandler} type='text' /><div className={styles.ekpixel}></div></div>)
+    return (<div className={styles.terminalcontainer}>{getColored("visitor14234@online:$ ", "#f48020")}<input style={{ width: width + 'ch' }} className={styles.terminput} autoFocus onKeyDown={changeHandler} id='terminput' type='text' /><div className={styles.ekpixel}></div></div>)
 }
 
 export default Terminput;
